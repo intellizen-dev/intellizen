30a4f647ddd37f68369b849c74d545ebd0d36723
WarmthDawn <warmth06@sina.com>
2024-09-12 20:50:23 +0800

    :feat: refactor type to class

--- packages/zenscript/src/typing/description.ts
+++ packages/zenscript/src/typing/description.ts
@@ -8,105 +8,153 @@ interface TypeFor<T> {
 }
 // endregion
 
-// region TypeDescription
-export type TypeDescription = FunctionTypeDescription | ClassTypeDescription | MapTypeDescription | PrimitiveTypeDescription<PrimitiveTypes> | MultiTypeDescription<MultiTypes>
 
-export interface PrimitiveTypeDescription<T extends PrimitiveTypes = 'any'> extends TypeFor<T> { }
+export class TypeDescription {
+  $type: string
 
-export interface FunctionTypeDescription<
-  P extends TypeDescription[] = TypeDescription[],
-  R extends TypeDescription = TypeDescription,
-> extends TypeFor<'function'> {
-  paramTypes: P
-  returnType: R
+  constructor($type: string) {
+    this.$type = $type
+  }
 }
 
-export interface ClassTypeDescription extends TypeFor<'class'> {
+export class PrimitiveTypeDescription extends TypeDescription {
+  constructor($type: PrimitiveTypes = 'any' as PrimitiveTypes) {
+    super($type)
+  }
+}
+
+export class FunctionTypeDescription extends TypeDescription {
+  paramTypes: TypeDescription[]
+  returnType: TypeDescription
+
+  constructor(paramTypes: TypeDescription[], returnType: TypeDescription) {
+    super('function')
+    this.paramTypes = paramTypes
+    this.returnType = returnType
+  }
+}
+
+export class ClassTypeDescription extends TypeDescription {
   className: string
+
+  constructor(className: string) {
+    super('class')
+    this.className = className
+  }
 }
 
-export interface MapTypeDescription<
-  K extends TypeDescription = TypeDescription,
-  V extends TypeDescription = TypeDescription,
-> extends TypeFor<'map'> {
-  keyType: K
-  valueType: V
+export class MapTypeDescription extends TypeDescription {
+  keyType: TypeDescription
+  valueType: TypeDescription
+
+  constructor(keyType: TypeDescription, valueType: TypeDescription) {
+    super('map')
+    this.keyType = keyType
+    this.valueType = valueType
+  }
 }
 
-export interface MultiTypeDescription<
-  N extends MultiTypes,
-  T = N extends Extract<MultiTypes, 'array' | 'list'> ? TypeDescription : TypeDescription[],
-> extends TypeFor<N> {
-  elementType: T
+export class ArrayTypeDescription extends TypeDescription {
+  elementType: TypeDescription
+  constructor(elementType: TypeDescription) {
+    super('array')
+    this.elementType = elementType
+  }
 }
+
+export class ListTypeDescription extends TypeDescription {
+  elementType: TypeDescription
+  constructor(elementType: TypeDescription) {
+    super('list')
+    this.elementType = elementType
+  }
+}
+
+export class UnionTypeDescription extends TypeDescription {
+  elementTypes: TypeDescription[]
+  constructor(elementTypes: TypeDescription[]) {
+    super('union')
+    this.elementTypes = elementTypes
+  }
+}
+
+export class IntersectionTypeDescription extends TypeDescription {
+  elementTypes: TypeDescription[]
+  constructor(elementTypes: TypeDescription[]) {
+    super('intersection')
+    this.elementTypes = elementTypes
+  }
+
+}
+
 // endregion
 
 // region Predicates
-export function isStringType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription<'string'> {
+export function isStringType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription {
   return typeDesc.$type === 'string'
 }
 
-export function isAnyType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription<'any'> {
+export function isAnyType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription {
   return typeDesc.$type === 'any'
 }
 
-export function isBoolType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription<'bool'> {
+export function isBoolType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription {
   return typeDesc.$type === 'bool'
 }
 
-export function isByteType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription<'byte'> {
+export function isByteType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription {
   return typeDesc.$type === 'byte'
 }
 
-export function isDoubleType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription<'double'> {
+export function isDoubleType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription {
   return typeDesc.$type === 'double'
 }
 
-export function isFloatType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription<'float'> {
+export function isFloatType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription {
   return typeDesc.$type === 'float'
 }
 
-export function isIntType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription<'int'> {
+export function isIntType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription {
   return typeDesc.$type === 'int'
 }
 
-export function isLongType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription<'long'> {
+export function isLongType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription {
   return typeDesc.$type === 'long'
 }
 
-export function isShortType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription<'short'> {
+export function isShortType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription {
   return typeDesc.$type === 'short'
 }
 
-export function isVoidType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription<'void'> {
+export function isVoidType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription {
   return typeDesc.$type === 'void'
 }
 
-export function isPrimitiveType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription<PrimitiveTypes> {
-  return ['string', 'any', 'bool', 'byte', 'double', 'float', 'int', 'long', 'short', 'void'].includes(typeDesc.$type)
+export function isPrimitiveType(typeDesc: TypeDescription): typeDesc is PrimitiveTypeDescription {
+  return typeDesc instanceof PrimitiveTypeDescription
 }
 
 export function isClassType(typeDesc: TypeDescription): typeDesc is ClassTypeDescription {
-  return typeDesc.$type === 'class'
+  return typeDesc instanceof ClassTypeDescription
 }
 
 export function isMapType(typeDesc: TypeDescription): typeDesc is MapTypeDescription {
-  return typeDesc.$type === 'map'
+  return typeDesc instanceof MapTypeDescription
 }
 
-export function isArrayType(typeDesc: TypeDescription): typeDesc is MultiTypeDescription<'array'> {
-  return typeDesc.$type === 'array'
+export function isArrayType(typeDesc: TypeDescription): typeDesc is ArrayTypeDescription {
+  return typeDesc instanceof ArrayTypeDescription
 }
 
-export function isListType(typeDesc: TypeDescription): typeDesc is MultiTypeDescription<'list'> {
-  return typeDesc.$type === 'list'
+export function isListType(typeDesc: TypeDescription): typeDesc is ListTypeDescription {
+  return typeDesc instanceof ListTypeDescription
 }
 
-export function isUnionType(typeDesc: TypeDescription): typeDesc is MultiTypeDescription<'union'> {
-  return typeDesc.$type === 'union'
+export function isUnionType(typeDesc: TypeDescription): typeDesc is UnionTypeDescription {
+  return typeDesc instanceof UnionTypeDescription
 }
 
-export function isIntersectionType(typeDesc: TypeDescription): typeDesc is MultiTypeDescription<'intersection'> {
-  return typeDesc.$type === 'intersection'
+export function isIntersectionType(typeDesc: TypeDescription): typeDesc is IntersectionTypeDescription {
+  return typeDesc instanceof IntersectionTypeDescription
 }
 // endregion
--- packages/zenscript/src/typing/factory.ts
+++ packages/zenscript/src/typing/factory.ts
@@ -1,22 +1,17 @@
-import type { ClassTypeDescription, FunctionTypeDescription, MapTypeDescription, MultiTypeDescription, MultiTypes, PrimitiveTypeDescription, PrimitiveTypes, TypeDescription } from './description'
+import { ArrayTypeDescription, ClassTypeDescription, FunctionTypeDescription, IntersectionTypeDescription, ListTypeDescription, MapTypeDescription, PrimitiveTypeDescription, TypeDescription, UnionTypeDescription } from './description'
+import type { PrimitiveTypes } from './description'
 
 export function createPrimitiveType<
   T extends PrimitiveTypes = 'any',
->($type: T = 'any' as T): PrimitiveTypeDescription<T> {
-  return {
-    $type,
-  }
+>($type: T = 'any' as T): PrimitiveTypeDescription {
+  return new PrimitiveTypeDescription($type)
 }
 
 export function createFunctionType<
   P extends TypeDescription[],
   R extends TypeDescription,
->(paramTypes: P, returnType: R): FunctionTypeDescription<P, R> {
-  return {
-    $type: 'function',
-    paramTypes,
-    returnType,
-  }
+>(paramTypes: P, returnType: R): FunctionTypeDescription {
+  return new FunctionTypeDescription(paramTypes, returnType)
 }
 
 export function createClassType(className: string): ClassTypeDescription {
@@ -29,20 +24,25 @@ export function createClassType(className: string): ClassTypeDescription {
 export function createMapType<
   K extends TypeDescription,
   V extends TypeDescription,
->(keyType: K, valueType: V): MapTypeDescription<K, V> {
-  return {
-    $type: 'map',
-    keyType,
-    valueType,
-  }
+>(keyType: K, valueType: V): MapTypeDescription {
+  return new MapTypeDescription(keyType, valueType)
 }
 
-export function createMultiType<
-  N extends MultiTypes,
-  T = N extends Extract<MultiTypes, 'array' | 'list'> ? TypeDescription : TypeDescription[],
->($type: N, elementType: T): MultiTypeDescription<N, T> {
-  return {
-    $type,
-    elementType,
-  }
+export function createArrayType<E extends TypeDescription>(elementType: E): ArrayTypeDescription {
+  return new ArrayTypeDescription(elementType)
 }
+
+export function createListType<E extends TypeDescription>(elementType: E): ListTypeDescription {
+  return new ListTypeDescription(elementType)
+}
+
+export function createUnionType(...elementTypes: TypeDescription[]): UnionTypeDescription {
+  return new UnionTypeDescription(elementTypes)
+}
+
+export function createIntersectionType(...elementTypes: TypeDescription[]): IntersectionTypeDescription {
+  return new IntersectionTypeDescription(elementTypes)
+}
+
+
+
