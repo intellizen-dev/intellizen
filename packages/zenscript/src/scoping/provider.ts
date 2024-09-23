d195d55f3a9f036523ec16a3f29cad8fa49f68a0
raylras <raylras@outlook.com>
2024-09-23 09:33:15 +0800

    Revert "minor"
    This reverts commit dfaa8d77e82d3f4a440b4d595f725f772d733228.

--- packages/zenscript/src/scoping/provider.ts
+++ packages/zenscript/src/scoping/provider.ts
@@ -56,7 +56,7 @@ export class ZenScriptScopeProvider extends DefaultScopeProvider {
     node.statements.filter(it => isVariableDeclaration(it))
       .filter(it => it.prefix === 'static')
       .forEach(it => members.push(it))
-    return members.map(this.createDescriptionForNode)
+    return members.map(it => this.createDescriptionForNode(it))
   }
 
   // region Declaration
@@ -93,7 +93,7 @@ export class ZenScriptScopeProvider extends DefaultScopeProvider {
     return getClassChain(node)
       .flatMap(it => it.members)
       .filter(it => isStaticMember(it))
-      .map(this.createDescriptionForNode)
+      .map(it => this.createDescriptionForNode(it))
   }
   // endregion
 
@@ -166,7 +166,7 @@ export class ZenScriptScopeProvider extends DefaultScopeProvider {
     return getClassChain(ref)
       .flatMap(it => it.members)
       .filter(it => !isStaticMember(it))
-      .map(this.createDescriptionForNode)
+      .map(it => this.createDescriptionForNode(it))
   }
   // endregion
 
