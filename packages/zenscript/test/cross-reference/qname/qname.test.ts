5352bc933514fc877057d517fd521e55fea8f79a
raylras <raylras@outlook.com>
2024-09-25 11:06:25 +0800

    test passed

--- packages/zenscript/test/cross-reference/qname/qname.test.ts
+++ packages/zenscript/test/cross-reference/qname/qname.test.ts
@@ -4,7 +4,7 @@ import { pathToFileURL } from 'node:url'
 import type { LangiumDocument } from 'langium'
 import { describe, expect, it } from 'vitest'
 import { createParseHelper } from '../../utils'
-import type { Script } from '../../../src/generated/ast'
+import type { ClassDeclaration, Script } from '../../../src/generated/ast'
 
 const parse = createParseHelper()
 
@@ -20,6 +20,13 @@ async function parseFile(filePath: string): Promise<LangiumDocument<Script>> {
 describe('test', () => {
   it('it', () => {
     const importDecl = b_zs.parseResult.value.imports[0]
-    expect(importDecl.refer.ref?.$container?.$document).toBe(a_zs)
+
+    const scripts = importDecl.path[0].ref as undefined
+    const a = importDecl.path[1].ref as Script
+    const Alpha = importDecl.path[2].ref as ClassDeclaration
+
+    expect(scripts).toBe(undefined)
+    expect(a.$document).toBe(a_zs)
+    expect(Alpha.name).toBe('Alpha')
   })
 })
