236fad0af8f8a3540e86245fd76f6cc7ea0eee4f
raylras <raylras@outlook.com>
2024-09-29 10:29:06 +0800

    improve testing

--- packages/zenscript/test/cross-reference/class-type/class-type.test.ts
+++ packages/zenscript/test/cross-reference/class-type/class-type.test.ts
@@ -4,12 +4,24 @@ import { pathToFileURL } from 'node:url'
 import { describe, expect, it } from 'vitest'
 import type { LangiumDocument } from 'langium'
 import { assertNoErrors, createParseHelper } from '../../utils'
-import type { ClassDeclaration, ClassType, Script, VariableDeclaration } from '../../../src/generated/ast'
+import type { ClassDeclaration, ClassType, ImportDeclaration, Script, VariableDeclaration } from '../../../src/generated/ast'
 
 const parse = createParseHelper()
+const document_provider_zs = await parseFile(path.resolve(__dirname, 'scripts', 'provider.zs'))
+const document_user_zs = await parseFile(path.resolve(__dirname, 'scripts', 'user.zs'))
 
-const provider_zs = await parseFile(path.resolve(__dirname, 'scripts', 'provider.zs'))
-const user_zs = await parseFile(path.resolve(__dirname, 'scripts', 'user.zs'))
+const script_provider_zs = document_provider_zs.parseResult.value
+const zenclass_alpha = script_provider_zs.classes[0] as ClassDeclaration
+
+const script_user = document_user_zs.parseResult.value
+const import_scripts_provider = script_user.imports[0] as ImportDeclaration
+const import_scripts_provider_as_supplier = script_user.imports[1] as ImportDeclaration
+const import_scripts_provider_alpha = script_user.imports[2] as ImportDeclaration
+const import_scripts_provider_alpha_as_beta = script_user.imports[3] as ImportDeclaration
+const var_script_be_imported = script_user.statements[0] as VariableDeclaration
+const var_script_be_imported_as_alias = script_user.statements[1] as VariableDeclaration
+const var_class_be_imported = script_user.statements[2] as VariableDeclaration
+const var_class_be_imported_as_alias = script_user.statements[3] as VariableDeclaration
 
 async function parseFile(filePath: string): Promise<LangiumDocument<Script>> {
   const content = readFileSync(filePath).toString()
@@ -19,43 +31,35 @@ async function parseFile(filePath: string): Promise<LangiumDocument<Script>> {
 
 describe('check cross reference of class type', () => {
   it('should no errors', () => {
-    assertNoErrors(provider_zs)
-    assertNoErrors(user_zs)
+    assertNoErrors(document_provider_zs)
+    assertNoErrors(document_user_zs)
   })
 
   it('script be imported', () => {
-    const script_be_imported = user_zs.parseResult.value.statements[0] as VariableDeclaration
-    expect(script_be_imported.$container?.$document).toBe(user_zs)
-
-    const referenced = (script_be_imported.typeRef as ClassType).path.at(-1)?.ref as ClassDeclaration
-    expect(referenced?.$container?.$document).toBe(provider_zs)
-    expect(referenced?.name).toBe('Alpha')
+    expect(import_scripts_provider.path[0].ref).toBeUndefined()
+    expect(import_scripts_provider.path[1].ref).toBe(script_provider_zs)
+    expect((var_script_be_imported.typeRef as ClassType).path[0].ref).toBe(script_provider_zs)
+    expect((var_script_be_imported.typeRef as ClassType).path[1].ref).toBe(zenclass_alpha)
   })
 
   it('script be imported as alias', () => {
-    const script_be_imported_as_alias = user_zs.parseResult.value.statements[1] as VariableDeclaration
-    expect(script_be_imported_as_alias.$container?.$document).toBe(user_zs)
-
-    const referenced = (script_be_imported_as_alias.typeRef as ClassType).path.at(-1)?.ref as ClassDeclaration
-    expect(referenced?.$container?.$document).toBe(provider_zs)
-    expect(referenced?.name).toBe('Alpha')
+    expect(import_scripts_provider_as_supplier.path[0].ref).toBeUndefined()
+    expect(import_scripts_provider_as_supplier.path[1].ref).toBe(script_provider_zs)
+    expect((var_script_be_imported_as_alias.typeRef as ClassType).path[0].ref).toBe(script_provider_zs)
+    expect((var_script_be_imported_as_alias.typeRef as ClassType).path[1].ref).toBe(zenclass_alpha)
   })
 
   it('class be imported', () => {
-    const class_be_imported = user_zs.parseResult.value.statements[2] as VariableDeclaration
-    expect(class_be_imported.$container?.$document).toBe(user_zs)
-
-    const referenced = (class_be_imported.typeRef as ClassType).path.at(-1)?.ref as ClassDeclaration
-    expect(referenced?.$container?.$document).toBe(provider_zs)
-    expect(referenced?.name).toBe('Alpha')
+    expect(import_scripts_provider_alpha.path[0].ref).toBeUndefined()
+    expect(import_scripts_provider_alpha.path[1].ref).toBe(script_provider_zs)
+    expect(import_scripts_provider_alpha.path[2].ref).toBe(zenclass_alpha)
+    expect((var_class_be_imported.typeRef as ClassType).path[0].ref).toBe(zenclass_alpha)
   })
 
   it('class be imported as alias', () => {
-    const class_be_imported_as_alias = user_zs.parseResult.value.statements[3] as VariableDeclaration
-    expect(class_be_imported_as_alias.$container?.$document).toBe(user_zs)
-
-    const referenced = (class_be_imported_as_alias.typeRef as ClassType).path.at(-1)?.ref as ClassDeclaration
-    expect(referenced?.$container?.$document).toBe(provider_zs)
-    expect(referenced?.name).toBe('Alpha')
+    expect(import_scripts_provider_alpha_as_beta.path[0].ref).toBeUndefined()
+    expect(import_scripts_provider_alpha_as_beta.path[1].ref).toBe(script_provider_zs)
+    expect(import_scripts_provider_alpha_as_beta.path[2].ref).toBe(zenclass_alpha)
+    expect((var_class_be_imported_as_alias.typeRef as ClassType).path[0].ref).toBe(zenclass_alpha)
   })
 })
