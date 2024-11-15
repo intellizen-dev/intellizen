09605f5fe4e6dff286e1a45de0d11228a1aec96d
raylras <raylras@outlook.com>
2024-11-15 17:44:05 +0800

    remove `onDispose`

--- packages/zenscript/src/utils/cache.ts
+++ packages/zenscript/src/utils/cache.ts
@@ -1,22 +1,10 @@
-import type { LangiumSharedCoreServices } from 'langium'
-import { ContextCache, SimpleCache, stream } from 'langium'
+import { ContextCache, SimpleCache } from 'langium'
 
 export class WorkspaceCache extends SimpleCache<object, ContextCache<object, any, any>> {
-  constructor(services: LangiumSharedCoreServices) {
+  constructor() {
     super()
     // @ts-expect-error replace with WeakMap
     this.cache = new WeakMap()
-
-    this.onDispose(services.workspace.DocumentBuilder.onUpdate((changed, deleted) => {
-      if (deleted.length > 0) {
-        this.clear()
-      }
-      else if (stream(changed)
-        .map(it => services.workspace.LangiumDocuments.getDocument(it)!)
-        .some(it => it.textDocument.version > 1)) {
-        this.clear()
-      }
-    }))
   }
 
   get(key: object) {
