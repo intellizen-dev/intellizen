60f6d05e94eee1e7ee7612bbbcfd52789c2e95f8
raylras <raylras@outlook.com>
2024-11-15 09:05:27 +0800

    workspace cache invalidation

--- packages/zenscript/src/utils/cache.ts
+++ packages/zenscript/src/utils/cache.ts
@@ -1,9 +1,22 @@
-import { ContextCache as LangiumContextCache, WorkspaceCache as LangiumWorkspaceCache } from 'langium'
+import type { LangiumSharedCoreServices } from 'langium'
+import { ContextCache, SimpleCache, stream } from 'langium'
 
-export class ContextCache extends LangiumContextCache<object, any, any> {}
+export class WorkspaceCache extends SimpleCache<object, ContextCache<object, any, any>> {
+  constructor(services: LangiumSharedCoreServices) {
+    super()
+    this.onDispose(services.workspace.DocumentBuilder.onUpdate((changed, deleted) => {
+      if (deleted.length > 0) {
+        this.clear()
+      }
+      else if (stream(changed)
+        .map(it => services.workspace.LangiumDocuments.getDocument(it)!)
+        .some(it => it.textDocument.version > 1)) {
+        this.clear()
+      }
+    }))
+  }
 
-export class WorkspaceCache extends LangiumWorkspaceCache<object, ContextCache> {
-  get(key: object): ContextCache {
+  get(key: object) {
     return super.get(key, () => new ContextCache())
   }
 }
