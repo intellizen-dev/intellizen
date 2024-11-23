eb990053c5deaef12c0e5eee8e1fca805f4605a4
raylras <raylras@outlook.com>
2024-11-23 11:08:54 +0800

    lint; renamed `RuleFunctionType`

--- packages/zenscript/src/utils/rule.ts
+++ packages/zenscript/src/utils/rule.ts
@@ -1,14 +1,13 @@
 type RuleMap = { [K in string]: (source: any, ...rest: any) => any }
-type RuleFunction<T extends RuleMap> = (source: any, ...rest: RestParameters<MemberType<T>>) => ReturnType<MemberType<T>> | undefined
-type MemberType<T> = T extends { [K in string]: infer V } ? (V extends (source: any, ...rest: any) => any ? V : never) : never
-type RestParameters<T> = T extends (head: any, ...tail: infer U) => any ? U : never
 type RuleGetter<M extends RuleMap> = ($type: string | undefined) => RuleFunction<M> | undefined
+type RuleFunction<M extends RuleMap> = (source: any, ...rest: RestParameters<RuleFunctionType<M>>) => ReturnType<RuleFunctionType<M>>
 
-export function defineRules<M extends RuleMap>(
-  rules: Partial<M>,
-): RuleGetter<M> {
+type RestParameters<T> = T extends (source: any, ...rest: infer U) => any ? U : never
+type RuleFunctionType<T> = T extends { [K in string]: infer V } ? (V extends (source: any, ...rest: any) => any ? V : never) : never
+
+export function defineRules<M extends RuleMap>(rules: Partial<M>): RuleGetter<M> {
+  // assign the name for anonymous rule function
   for (const [$type, ruleFn] of Object.entries(rules)) {
-    // assign the name for anonymous rule function
     if (!('name' in ruleFn)) {
       Object.defineProperty(ruleFn, 'name', { value: $type, writable: false })
     }
